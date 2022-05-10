e3f521f8a0546004d3b7a3c5691893756646a0a8
Carlos <carlosemaya@gmail.com>
2022-05-10 20:49:43 -0300

    Next Button

--- app/js/newgame.js
+++ app/js/newgame.js
@@ -3,29 +3,26 @@ const choices = Array.from(document.querySelectorAll('.choice-text')); // Array 
 const progressText = document.querySelector('#progressText'); // Perguntas/Total de perguntas
 const scoreText = document.querySelector('#score'); // Pontuação
 const progressBarFull = document.querySelector('#progressBarFull'); // Barra de progresso
+const opcao = Array.from(document.querySelectorAll('.opcao'));
 const opcao1 = document.querySelector('#opcao-1'); // Opção radio
 const opcao2 = document.querySelector('#opcao-2'); // Opção radio
 const opcao3 = document.querySelector('#opcao-3'); // Opção radio
 const opcao4 = document.querySelector('#opcao-4'); // Opção radio
 const proximoBtn = document.querySelector('#proximo-btn'); // Botão próximo
 
-var opcoes = [opcao1, opcao2, opcao3, opcao4]
-var opcoesLength = opcoes.length
-var i = 0;
-let currentQuestion = {}
-let acceptingAnswers = true
-let score = 0
-let questionCounter = 0
-let availableQuestions = []
+let perguntaAtual = {} // Lista
+let aceitandoRespostas = true
+let pontuacao = 0
+let contadorPerguntas = 0
+let perguntasDisponiveis = [] // Array
 
-let questions = [
+let perguntas = [
     {
         question: "Eu sou...",
         choice1: "Idealista, criativo e visionário.",
         choice2: "Divertido, espiritual e benéfico",
         choice3: "Confiável, meticuloso e previsível",
         choice4: "Focado, determinado e persistente",
-        answer: 2,
     },
     {
         question: "Eu gosto de...",
@@ -33,7 +30,6 @@ let questions = [
         choice2: "Conversar com os passageiros.",
         choice3: "Planejar a viagem.",
         choice4: "Explorar novas rotas.",
-        answer: 2,
     },
     {
         question: "Se você quiser se dar bem comigo...",
@@ -41,7 +37,6 @@ let questions = [
         choice2: "Me deixe saber sua expectativa.",
         choice3: "Lidere, siga ou saia do caminho.",
         choice4: "Seja amigável, carinhoso e compreensivo.",
-        answer: 2,
     },
     {
         question: "Para conseguir obter bons resultados é preciso...",
@@ -49,7 +44,6 @@ let questions = [
         choice2: "Controlar o essencial.",
         choice3: "Diversão e celebração.",
         choice4: "Planejar e obter recursos.",
-        answer: 2,
     },
     {
         question: "Eu me divirto quando...",
@@ -57,7 +51,6 @@ let questions = [
         choice2: "Tenho novidades.",
         choice3: "Estou com outros.",
         choice4: "Determino as regras.",
-        answer: 2,
     },
     {
         question: "Eu penso que...",
@@ -65,7 +58,6 @@ let questions = [
         choice2: "O ataque é melhor que a defesa.",
         choice3: "É bom ser manso, mas andar com um porrete.",
         choice4: "Um homem previnido vale por dois.",
-        answer: 2,
     },
     {
         question: "Minha preocupação é...",
@@ -73,7 +65,6 @@ let questions = [
         choice2: "Fazer com que as pessoas gostem.",
         choice3: "Fazer com que funcione.",
         choice4: "Fazer com que aconteça.",
-        answer: 2,
     },
     {
         question: "Eu prefiro...",
@@ -81,7 +72,6 @@ let questions = [
         choice2: "Ter todos os detalhes.",
         choice3: "Vantagens ao meu favor.",
         choice4: "Que todos tenham a chance de serem ouvidos.",
-        answer: 2,
     },
     {
         question: "Eu gosto de...",
@@ -89,7 +79,6 @@ let questions = [
         choice2: "Construir memórias.",
         choice3: "Fazer sentido.",
         choice4: "Tornar as pessoas confortáveis.",
-        answer: 2,
     },
     {
         question: "Eu gosto de chegar...",
@@ -97,7 +86,6 @@ let questions = [
         choice2: "Junto.",
         choice3: "Na hora.",
         choice4: "Em outro lugar.",
-        answer: 2,
     },
     {
         question: "Um ótimo dia para mim é quando...",
@@ -105,7 +93,6 @@ let questions = [
         choice2: "Me divirto com meus amigos.",
         choice3: "Tudo segue conforme o planejado.",
         choice4: "Desfruto de coisas novas e estimulantes.",
-        answer: 2,
     },
     {
         question: "Eu vejo a morte como...",
@@ -113,7 +100,6 @@ let questions = [
         choice2: "Oportunidade para rever os falecidos.",
         choice3: "Um modo de receber recompensas.",
         choice4: "Algo que sempre chega muito cedo.",
-        answer: 2,
     },
     {
         question: "Minha filosofia de vida é...",
@@ -121,7 +107,6 @@ let questions = [
         choice2: "Para eu ganhar, ninguém precisa perder.",
         choice3: "Para ganhar é preciso seguir as regras.",
         choice4: "Para ganhar, é necessário inventar novas regras.",
-        answer: 2,
     },
     {
         question: "Eu sempre gostei de...",
@@ -129,7 +114,6 @@ let questions = [
         choice2: "Evitar surpresas.",
         choice3: "Focalizar a meta.",
         choice4: "Realizar uma abordagem natural.",
-        answer: 2,
     },
     {
         question: "Eu gosto de mudanças se...",
@@ -137,7 +121,6 @@ let questions = [
         choice2: "For divertido e puder ser compartilhado",
         choice3: "Me der mais liberdade e variedade.",
         choice4: "Melhorar ou me der mais controle.",
-        answer: 2,
     },
     {
         question: "Eu gosto de mudanças se...",
@@ -145,7 +128,6 @@ let questions = [
         choice2: "Se colocar os outros na frente.",
         choice3: "Mudar de ideia.",
         choice4: "Ser consistente.",
-        answer: 2,
     },
     {
         question: "Eu gosto de buscar conselhos de...",
@@ -153,7 +135,6 @@ let questions = [
         choice2: "Anciões e conselheiros.",
         choice3: "Autoridades no assunto.",
         choice4: "Lugares, os mais estranhos.",
-        answer: 2,
     },
     {
         question: "Meu lema é...",
@@ -161,7 +142,6 @@ let questions = [
         choice2: "Fazer bem feito.",
         choice3: "Fazer junto com o grupo.",
         choice4: "Simplesmente fazer.",
-        answer: 2,
     },
     {
         question: "Eu gosto de...",
@@ -169,7 +149,6 @@ let questions = [
         choice2: "Ordem e sistematização.",
         choice3: "Calor humano e animação.",
         choice4: "Coisas claras e simples.",
-        answer: 2,
     },
     {
         question: "Tempo para mim é...",
@@ -177,7 +156,6 @@ let questions = [
         choice2: "Um grande ciclo.",
         choice3: "Uma flecha que leva ao inevitável.",
         choice4: "Irrelevante.",
-        answer: 2,
     },
     {
         question: "Se eu fosse bilionário...",
@@ -185,7 +163,6 @@ let questions = [
         choice2: "Criaria uma poupança avantajada.",
         choice3: "Faria o que desse na cabeça.",
         choice4: "Me exibiria bastante para algumas pessoas.",
-        answer: 2,
     },
     {
         question: "Eu acredito que...",
@@ -193,7 +170,6 @@ let questions = [
         choice2: "A jornada é mais importante que o destino.",
         choice3: "Um centavo economizado é um centavo ganho.",
         choice4: "Basta um navio e uma estrela para navegar.",
-        answer: 2,
     },
     {
         question: "Eu acredito também que...",
@@ -201,7 +177,6 @@ let questions = [
         choice2: "De grão em grão a galinha enche o papo.",
         choice3: "O que vai, volta.",
         choice4: "Um sorriso ou uma careta é o mesmo para quem é cego.",
-        answer: 2,
     },
     {
         question: "Eu acredito ainda que...",
@@ -209,7 +184,6 @@ let questions = [
         choice2: "A autoridade deve ser desafiada.",
         choice3: "Ganhar é fundamental.",
         choice4: "O coletivo é mais importante que o individual.",
-        answer: 2,
     },
     {
         question: "Eu penso que...",
@@ -217,136 +191,102 @@ let questions = [
         choice2: "É preferível olhar, antes de pular.",
         choice3: "Duas cabeças pensam melhor do que uma.",
         choice4: "Se você não tem condições de competir, não compita.",
-        answer: 2,
     },
 ]
 
-const SCORE_POINTS = 25 // Pontos recebidos por pergunta certa
-const MAX_QUESTIONS = 25 // Máximo de perguntas
+const MAX_PERGUNTAS = 25;
 
-console.log(opcoes.checked)
+falaTagarela = () => {
+    if(opcao1.checked === true){
+        console.log('checado')
+    } else {
+        console.log('não checado')
+    }
+}
 
-startGame = () => {
-    questionCounter = 0
-    score = 0
-    availableQuestions = [...questions]
-    getNewQuestion()
+iniciarJogo = () => {
+    contadorPerguntas = 0
+    perguntasDisponiveis = [...perguntas]
+    novaPergunta()
 }
 
 stopDefault = () => {
     proximoBtn.addEventListener("click", function(event){
-        event.preventDefault() 
+        event.preventDefault()
     });
 }
 
-acceptAnswer = () => {
-    //proximoBtn.preventDefault()
-    acceptingAnswers = true
-    getNewQuestion()
+aceitarResposta = () => {
+    aceitandoRespostas = true
+    if(opcao1.checked === true) {
+        novaPergunta()
+    }
+    if(opcao2.checked === true) {
+        novaPergunta()
+    }
+    if(opcao3.checked === true) {
+        novaPergunta()
+    }
+    if(opcao4.checked === true) {
+        novaPergunta()
+    }
 }
 
 enableDisable = () => {
-    
-    proximoBtn.disabled = !opcao1.checked
+    /*proximoBtn.disabled = !opcao1.checked
     proximoBtn.disabled = !opcao2.checked
     proximoBtn.disabled = !opcao3.checked
-    proximoBtn.disabled = !opcao4.checked
-    
+    proximoBtn.disabled = !opcao4.checked*/
+    console.log(opcao)
 }
 
-getNewQuestion = () => { // Buscar nova pergunta
+novaPergunta = () => {
 
-    acceptingAnswers = false;
-    //,proximoBtn.disabled = true;
-    
+    aceitandoRespostas = false
+
     opcao1.checked = false;
     opcao2.checked = false;
     opcao3.checked = false;
     opcao4.checked = false;
 
-    if(availableQuestions.length === 0 || questionCounter > MAX_QUESTIONS) {
-        localStorage.setItem('mostRecentScore', score) // Adiciona a pontuação para armazenamento
+    if(perguntasDisponiveis.length === 0 || contadorPerguntas > MAX_PERGUNTAS) {
+        console.log('Fim do Jogo')
 
         return window.location.assign('/end.html')
     }
-
-    // Adiciona +1 no contador de perguntas
-    // Barra de progresso é preenchida conforme o contador aumenta
-
-    questionCounter++
-    console.log(questionCounter)
-    progressText.innerText = `${questionCounter} / ${MAX_QUESTIONS}`
-    //progressBarFull.style.width = `${(questionCounter/MAX_QUESTIONS) * 100}%`
     
-    //const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
-    const questionsIndex = 0
-    currentQuestion = availableQuestions[questionsIndex]
-    question.innerText = currentQuestion.question
+    contadorPerguntas++
+    progressText.innerText = `${contadorPerguntas}/${MAX_PERGUNTAS}`
+
+    const indexPerguntas = 0
+    perguntaAtual = perguntasDisponiveis[indexPerguntas]
+    question.innerText = perguntaAtual.question // Puxando a question da array de perguntas
 
     choices.forEach(choice => {
         const number = choice.dataset['number']
-        choice.innerText = currentQuestion['choice' + number]
+        choice.innerText = perguntaAtual['choice' + number]
     })
 
-    availableQuestions.splice(questionsIndex, 1)
+    perguntasDisponiveis.splice(indexPerguntas, 1) // Remove a pergunta index 0
 
-    /*opcoes.forEach(function (item, index) {
-        if(item.checked == true) {
-            proximoBtn.disabled = false;
-        }
-        else {
-            proximoBtn.disabled = true;
-        }
-    });*/
-    
-    /*for (i; i < opcoesLength; i++) {
-        if(opcoes.checked) {
-            //proximoBtn.disabled = false;
-            console.log(opcoes[i])
-        }
-        
-    }*/
-    
-    //acceptingAnswers = true
+    //aceitandoRespostas = true
+
     stopDefault()
-
-    if(questionsIndex > 0) {
-        questionsIndex++
-    }
-
-    
 }
 
-
-
 choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        if(!acceptingAnswers) return
+    choice.addEventListener('click', e => { // Para cada escolha
+        if(!aceitandoRespostas) return
 
-        acceptingAnswers = false
-        const selectedChoice = e.target
+        aceitandoRespostas = false
+        const selectedChoice = e.target // Seleciona a resposta clicada
         const selectedAnswer = selectedChoice.dataset['number']
 
-        let classToApply = selectedAnswer  /* == currentQuestion.answer ? 'correct' : 'incorrect' */
-
-        if(classToApply === selectedAnswer) {
-            incrementScore(SCORE_POINTS)
-        }
-
-        //selectedChoice.parentElement.classList.add(classToApply)
-
         setTimeout(() => {
-            //selectedChoice.parentElement.classList.remove(classToApply)
-            getNewQuestion()
-
+            novaPergunta()
         }, 1000)
     })
 })
 
-incrementScore = num => {
-    score +=num
-    //scoreText.innerText = score
-}
+iniciarJogo()
 
-startGame()
-//
